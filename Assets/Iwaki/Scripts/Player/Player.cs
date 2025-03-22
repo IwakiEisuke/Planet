9a64b749df0fd2b23a3eed0334d8a7f915ee4194
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-03-22 18:19:36 +0900

    Player: 接地判定

--- Assets/Iwaki/Scripts/Player/Player.cs
+++ Assets/Iwaki/Scripts/Player/Player.cs
@@ -13,10 +13,12 @@ public class Player : MonoBehaviour
     [Header("Check Grounded")]
     [SerializeField] LayerMask groundedLayer;
     [SerializeField] float rayDistance = 1;
+    [SerializeField] float canGroundedAngle = 45;
 
     Rigidbody2D _rb;
 
     Vector2 _input;
+    bool _isGrounded;
 
     private void Start()
     {
@@ -26,6 +28,27 @@ public class Player : MonoBehaviour
     private void Update()
     {
         _rb.linearVelocityX = Mathf.MoveTowards(_rb.linearVelocityX, _input.x * speed, accel * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        IsGrounded(collision);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        IsGrounded(collision);
+    }
+
+    void IsGrounded(Collision2D collision)
+    {
+        foreach (var contact in collision.contacts)
+        {
+            if (Vector2.Dot(Vector2.up, contact.normal) >= Mathf.Cos(canGroundedAngle * Mathf.Deg2Rad))
+            {
+                _isGrounded = true;
+            }
+        }
     }
 
     void OnMove(InputValue value)
@@ -39,11 +62,14 @@ public class Player : MonoBehaviour
     {
         print("Jump");
 
+        if (!_isGrounded) return;
+
         var hit = Physics2D.Raycast(transform.position, Vector2.down, rayDistance, groundedLayer.value);
         if (hit)
         {
             print("Hit : " + hit.collider.name);
             _rb.linearVelocityY = Mathf.Sqrt(2 * Mathf.Abs(Physics.gravity.y) * jumpHeight);
+            _isGrounded = false;
         }
         else
         {
