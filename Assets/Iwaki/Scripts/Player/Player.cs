d9fd8dc0095bbadb4465c299bff1a10bdcdadf2b
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-03-26 22:10:52 +0900

    急斜面に対して崖掴み判定が出てしまうのを修正

--- Assets/Iwaki/Scripts/Player/Player.cs
+++ Assets/Iwaki/Scripts/Player/Player.cs
@@ -222,6 +222,8 @@ public class Player : MonoBehaviour
         var wallHit = Physics2D.Raycast(origin, Vector2.right * PlayerDirectionNormalized, _ledgeHangingWidth, groundedLayer.value);
         var floorHit = Physics2D.Raycast(crossPoint + Vector2.up * _ledgeHangingHeight, Vector2.down, _ledgeHangingHeight, groundedLayer.value);
 
+        if (Vector2.Dot(Vector2.up, floorHit.normal) < Mathf.Cos(canGroundedAngle * Mathf.Deg2Rad)) return;
+
         if (floorHit.distance > 0.01f && wallHit.collider && floorHit.collider)
         {
             print($"Hanging <wall:{wallHit.collider.name}> <floor:{floorHit.collider.name}>");
